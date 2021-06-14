00a2b3a70a5f75eda2079904e539a1b9b0dbfffc
David <arctdav@gmail.com>
2021-06-13 23:54:17 -0400

    complete

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,15 @@
+import 'dart:async';
+import 'dart:convert';
+import 'dart:io';
+
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
 
 void main() {
-  runApp(MyApp());
+  runApp(Root());
 }
 
-class MyApp extends StatelessWidget {
+class Root extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
@@ -22,13 +27,56 @@ class MyApp extends StatelessWidget {
         // is not restarted.
         primarySwatch: Colors.blue,
       ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      debugShowCheckedModeBanner: false,
+      home: Joke(title: 'Random Joke Generator'),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key? key, required this.title}) : super(key: key);
+Future<Map<String, dynamic>> get(String url) async {
+  print('Api Get, url $url');
+  var responseJson;
+  try {
+    Uri uri = Uri.parse(url);
+    final response = await http.get(uri);
+    responseJson = _returnResponse(response);
+  } on SocketException {
+    print('No net');
+  }
+  print('api get received!');
+  return responseJson;
+}
+
+Map<String, dynamic>? _returnResponse(http.Response response) {
+  switch (response.statusCode) {
+    case 200:
+      Map<String, dynamic> responseJson = jsonDecode(response.body.toString());
+      print(responseJson);
+      return responseJson;
+    default:
+      print(
+          'Error occured while Communication with Server with StatusCode : ${response.statusCode}');
+  }
+}
+
+class JokeData {
+  JokeData({required this.setup, required this.delivery});
+
+  String? setup = '';
+  String? delivery = '';
+
+  String toString() {
+    return "${setup}, ${delivery}";
+  }
+
+  JokeData.fromJson(Map<String, dynamic> json) {
+    this.setup = json['setup']!=null ? json['setup'] : null;
+    this.delivery = json['delivery']!=null ? json['delivery'] : null;
+  }
+}
+
+class Joke extends StatefulWidget {
+  Joke({Key? key, required this.title}) : super(key: key);
 
   // This widget is the home page of your application. It is stateful, meaning
   // that it has a State object (defined below) that contains fields that affect
@@ -42,71 +90,127 @@ class MyHomePage extends StatefulWidget {
   final String title;
 
   @override
-  _MyHomePageState createState() => _MyHomePageState();
+  _JokeState createState() => _JokeState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+class _JokeState extends State<Joke> {
+  late JokeData _data;
+  String _joke = 'Loading';
+  String _delivery = 'Loading';
+  String _category = 'Programming';
 
-  void _incrementCounter() {
+  @override
+  void initState() {
+    super.initState();
+
+    _generate();
+  }
+
+  Future _generate() async {
+    final response = await get("https://v2.jokeapi.dev/joke/" + _category);
+    print(response.toString());
+    _data = JokeData.fromJson(response);
+    if(_data.delivery == null || _data.setup == null) {
+      print("ERROR - restarting Joke due to missing key data ===================================");
+      setState(() {
+        _joke = "Loading...";
+        _delivery = "Loading...";
+      });
+      _generate();
+      return;
+    }
+    print("setup: ${_data.setup}");
+    print("delivery: ${_data.delivery}");
     setState(() {
       // This call to setState tells the Flutter framework that something has
       // changed in this State, which causes it to rerun the build method below
       // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
+      // _joke without calling setState(), then the build method would not be
       // called again, and so nothing would appear to happen.
-      _counter++;
+      _joke = "${_data.setup}";
+      _delivery = "${_data.delivery}";
     });
   }
 
   @override
   Widget build(BuildContext context) {
     // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
+    // by the _generate method above.
     //
     // The Flutter framework has been optimized to make rerunning build methods
     // fast, so that you can just rebuild anything that needs updating rather
     // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
+        // Here we take the value from the Joke object that was created by
         // the App.build method, and use it to set our appbar title.
         title: Text(widget.title),
       ),
       body: Center(
         // Center is a layout widget. It takes a single child and positions it
         // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
+        child: Container(
+          margin: EdgeInsets.all(30.0),
+          child: Column(
+            // Column is also a layout widget. It takes a list of children and
+            // arranges them vertically. By default, it sizes itself to fit its
+            // children horizontally, and tries to be as tall as its parent.
+            //
+            // Invoke "debug painting" (press "p" in the console, choose the
+            // "Toggle Debug Paint" action from the Flutter Inspector in Android
+            // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
+            // to see the wireframe for each widget.
+            //
+            // Column has various properties to control how it sizes itself and
+            // how it positions its children. Here we use mainAxisAlignment to
+            // center the children vertically; the main axis here is the vertical
+            // axis because Columns are vertical (the cross axis would be
+            // horizontal).
+            crossAxisAlignment: CrossAxisAlignment.center,
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: <Widget>[
+              DropdownButton<String>(
+                value: _category,
+                icon: const Icon(Icons.arrow_downward),
+                iconSize: 24,
+                elevation: 16,
+                style: const TextStyle(color: Colors.deepPurple),
+                underline: Container(
+                  height: 2,
+                  color: Colors.deepPurpleAccent,
+                ),
+                onChanged: (String? newValue) {
+                  setState(() {
+                    _category = newValue!;
+                  });
+                  _generate();
+                },
+                items: <String>['Any', 'Misc', 'Programming', 'Dark', 'Pun', 'Spooky', 'Christmas']
+                    .map<DropdownMenuItem<String>>((String value) {
+                  return DropdownMenuItem<String>(
+                    value: value,
+                    child: Text(value),
+                  );
+                }).toList(),
+              ),
+              SizedBox(height: 100),
+              Text(
+                '$_joke',
+                style: Theme.of(context).textTheme.headline4,
+              ),
+              SizedBox(height: 30),
+              Text(
+                '$_delivery',
+                style: Theme.of(context).textTheme.headline4,
+              ),
+            ],
+          ),
         ),
       ),
       floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
+        onPressed: _generate,
+        tooltip: 'Generate',
+        child: Icon(Icons.calculate),
       ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
